cf61d0cfc35e472c49d6986ab025329648878777
diashelter <helter.dias@gmail.com>
2023-11-13 20:30:05 -0300

    add files aula_02

--- backend/ride/src/api.ts
+++ backend/ride/src/api.ts
@@ -1,22 +1,57 @@
-// @ts-nocheck
 import express from "express";
-import { calculate } from "./RideCalculator";
-import Ride from "./Ride";
+import CalculateRide from "./application/usecase/CalculateRide";
+import CreatePassenger from "./application/usecase/CreatePassenger";
+import CreateDriver from "./application/usecase/CreateDriver";
+import GetPassenger from "./application/usecase/GetPassenger";
+import GetDriver from "./application/usecase/GetDriver";
+import DriverRepositoryDatabase from "./infra/repository/DriverRepositoryDatabase";
+import PassengerRepositoryDatabase from "./infra/repository/PassengerRepositoryDatabase";
+
+// driver, primary actor, inbound adapter
 const app = express();
 
 app.use(express.json());
 
-app.post("/calculate_ride", function (req, res) {
+app.post("/calculate_ride", async function (req, res) {
 	try {
-		const ride = new Ride();
-		for (const segment of req.body.segments) {
-			ride.addSegment(segment.distance, new Date(segment.date));
-		}
-		const price = ride.calculate();
-		res.json({ price });
-	} catch (e) {
+		const usecase = new CalculateRide();
+		const output = await usecase.execute(req.body);
+		res.json(output);
+	} catch (e: any) {
 		res.status(422).send(e.message);
 	}
+});
+
+app.post("/passengers", async function (req, res) {
+	try {
+		const usecase = new CreatePassenger(new PassengerRepositoryDatabase());
+		const output = await usecase.execute(req.body);
+		res.json(output);
+	} catch (e: any) {
+		res.status(422).send(e.message);
+	}
+});
+
+app.get("/passengers/:passengerId", async function (req, res) {
+	const usecase = new GetPassenger(new PassengerRepositoryDatabase());
+	const output = await usecase.execute({ passengerId: req.params.passengerId });
+	res.json(output);
+});
+
+app.post("/drivers", async function (req, res) {
+	try {
+		const usecase = new CreateDriver(new DriverRepositoryDatabase());
+		const output = await usecase.execute(req.body);
+		res.json(output);
+	} catch (e: any) {
+		res.status(422).send(e.message);
+	}
+});
+
+app.get("/drivers/:driverId", async function (req, res) {
+	const usecase = new GetDriver(new DriverRepositoryDatabase());
+	const output = await usecase.execute({ driverId: req.params.driverId });
+	res.json(output);
 });
 
 app.listen(3000);
